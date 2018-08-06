bc893794c5e7ec1a968c00a8538241278e8c8d34
Kelly Rauchenberger <fefferburbia@gmail.com>
2018-08-06 16:06:12 -0400

    User objects now contain whether the user is protected

--- src/user.cpp
+++ src/user.cpp
@@ -4,7 +4,7 @@
 #include "client.h"
 
 namespace twitter {
-  
+
   user::user(std::string data) try
     : _valid(true)
   {
@@ -12,6 +12,7 @@ namespace twitter {
     _id = json["id"].get<user_id>();
     _screen_name = json["screen_name"].get<std::string>();
     _name = json["name"].get<std::string>();
+    _protected = json["protected"].get<bool>();
   } catch (const std::invalid_argument& error)
   {
     std::throw_with_nested(malformed_object("user", data));
--- src/user.h
+++ src/user.h
@@ -6,56 +6,64 @@
 #include <cassert>
 
 namespace twitter {
-  
+
   class client;
-  
+
   typedef unsigned long long user_id;
-  
+
   class user {
     public:
-      
+
       user() {}
       user(std::string data);
-      
+
       user_id getID() const
       {
         assert(_valid);
-        
+
         return _id;
       }
-      
+
       std::string getScreenName() const
       {
         assert(_valid);
-        
+
         return _screen_name;
       }
-      
+
       std::string getName() const
       {
         assert(_valid);
-        
+
         return _name;
       }
-      
+
+      bool isProtected() const
+      {
+        assert(_valid);
+
+        return _protected;
+      }
+
       bool operator==(const user& other) const
       {
         return _id == other._id;
       }
-      
+
       bool operator!=(const user& other) const
       {
         return _id != other._id;
       }
-      
+
     private:
-      
+
       bool _valid = false;
       user_id _id;
       std::string _screen_name;
       std::string _name;
+      bool _protected = false;
   };
-  
+
 };
 
 #endif /* end of include guard: USER_H_BF3AB38C */
